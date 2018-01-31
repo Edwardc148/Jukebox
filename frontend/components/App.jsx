a8c68c3ae2dda969c555bc74edb0b6dc7809007e
Edward <edwardcheng148@gmail.com>
2018-01-30 21:58:42 -0800

    made files to complete one redux cycle

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>This is now my Jukebox</h1>
+  </div>
+);
+
+export default App;
--- frontend/jukebox.jsx
+++ frontend/jukebox.jsx
@@ -4,9 +4,9 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const store = configureStore();
-  // window.store = store;
-  // window.getState = store.getState;
+  const store = configureStore();
+  window.store = store;
+  window.getState = store.getState;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome To Jukebox (Second Try)</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
