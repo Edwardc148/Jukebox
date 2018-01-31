79254544e12028853f92d8d2d22ba4fc48967f52
Edward <edwardcheng148@gmail.com>
2018-01-31 10:30:44 -0800

    add complete user auth

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import SignupContainer from './session/signup_container';
 import NavBarContainer from './navbar/nav_bar_container';
+import StationsIndexContainer from './stations/stations_index_container';
 import { Route } from 'react-router-dom';
+import {
+  AuthRoute,
+  ProtectedRoute,
+} from '../utils/route_utils';
 
 const App = () => (
   <div>
     <Route path="/" component={NavBarContainer} />
-    <Route path="/signup" component={SignupContainer} />
+    <AuthRoute path="/signup" component={SignupContainer} />
+    <ProtectedRoute path="/stations" component={StationsIndexContainer} />
   </div>
 );
 
