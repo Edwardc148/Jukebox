20c2679bc6094d0b3592210ad3bd317e72e8d529
Edward <edwardcheng148@gmail.com>
2018-01-30 21:36:35 -0800

    add actions and reducers for frontend auth

--- frontend/jukebox.jsx
+++ frontend/jukebox.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.store = store;
+  window.getState = store.getState;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome To Jukebox (Second Try)</h1>, root);
 });
