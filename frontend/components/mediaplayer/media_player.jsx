b14c48942226bf8d8cb8f181f3c40bca35806ed8
Edward <edwardcheng148@gmail.com>
2018-02-02 15:02:11 -0800

    add changes to media player

--- frontend/components/mediaplayer/media_player.jsx
+++ frontend/components/mediaplayer/media_player.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
-export default () => {
-  return (
-    <footer className="media-player">
-      <a href="https://github.com/Edwardc148/Jukebox">Github</a>
-    </footer>
-  );
-};
+class MediaPlayer extends React.Component {
+  render() {
+    return (
+      <div className="media-player">
+        <ReactPlayer
+          className="react-player" url="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3" playing controls volume />
+        <footer>
+          <a href="https://github.com/Edwardc148/Jukebox">Github</a>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MediaPlayer;
