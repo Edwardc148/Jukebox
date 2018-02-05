b18e375cb14e4b479d3fcc33b504115678e38da7
Edward <edwardcheng148@gmail.com>
2018-02-05 11:31:18 -0800

    added duration to media player

--- frontend/components/mediaplayer/react-player.jsx
+++ frontend/components/mediaplayer/react-player.jsx
@@ -5,15 +5,31 @@ class ReactMediaPlayer extends React.Component {
   constructor(props) {
     super(props);
     this.state = this.props;
+    this.onProgress = this.onProgress.bind(this);
+    this.onDuration = this.onDuration.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
     this.setState({playback: newProps.playback});
   }
 
+  onProgress(state) {
+    this.setState({'playedSeconds': state.playedSeconds});
+  }
+
+  onDuration(duration) {
+    this.setState({'duration': duration});
+  }
+
+  ref(){
+     return player => {
+       this.player = player;
+     };
+   }
+
   render (){
     let playbutton;
-
+    console.log(this.state);
     return (
       <div className="react-player-div">
         <div className='react-player-container'>
@@ -21,18 +37,28 @@ class ReactMediaPlayer extends React.Component {
             <a href="https://github.com/Edwardc148/Jukebox">Github</a>
           </footer>
           <ReactPlayer
+            ref={this.ref()}
             className="react-player"
             url={this.state.playback.current_song_url}
             width='0%'
             height='0%'
             playing={this.props.playback.playing}
+            onProgress={this.onProgress}
+            onDuration={this.onDuration}
           />
           <div className="button-div">
             <div className="playback-buttons" >
-              <span className="repeat"><i className="fas fa-redo-alt fa-2x"></i></span>
-              {this.state.playback.playing ? <span onClick={this.props.togglePlayPause}><i className="fas fa-pause fa-2x" onClick={this.props.togglePlayPause} ></i></span> : <span onClick={this.props.togglePlayPause}><i className="fas fa-play fa-2x" onClick={this.props.togglePlayPause} ></i></span>
-              }
-              <span className="next-button" onClick={this.props.nextSong}><i className="fas fa-fast-forward fa-2x"></i></span>
+              <span className="repeat"><i className="fas fa-redo-alt fa-2x repeat"></i></span>
+
+
+          {this.state.playback.playing ?
+            <span onClick={this.props.togglePlayPause}><i className="fas fa-pause fa-2x play-button" onClick={this.props.togglePlayPause} ></i></span>
+              :
+            <span onClick={this.props.togglePlayPause}><i className="fas fa-play fa-2x play-button" onClick={this.props.togglePlayPause} ></i></span>
+          }
+              <span className="next-button" onClick={this.props.nextSong}><i className="fas fa-fast-forward fa-2x next-button"></i></span>
+              <span>{this.state.playedSeconds}</span>
+              <span>{this.state.duration}</span>
             </div>
           </div>
         </div>
